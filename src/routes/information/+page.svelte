56e27bac6db90ec845ce80ba926d79ffe3bbd101
Yugandhar182 <yugandhar@recruitly.io>
2023-09-12 11:13:00 +0530

    .

--- src/routes/information/+page.svelte
+++ src/routes/information/+page.svelte
@@ -1,12 +1,26 @@
+<script>
+  import { onMount } from 'svelte';
 
+  let isBlue = true; // Start with blue color
 
-<h1 style="color: blue;" class="h1" >Welcome</h1>
+  onMount(() => {
+    // Create an interval to toggle the color every 2 seconds
+    const interval = setInterval(() => {
+      isBlue = !isBlue;
+    }, 1000);
+
+    // Clean up the interval when the component is unmounted
+    return () => clearInterval(interval);
+  });
+</script>
+
+<h1 style="color: {isBlue ? 'blue' : 'green'};" class="h1">Welcome</h1>
 
 <style>
-.h1{
-    margin-left: 500px;
-    margin-top: 35px;
+  .h1 {
+    margin-left: 600px;
+    margin-top: 195px;
     font-weight: bold;
-    font-size:30px;
+    font-size: 70px;
   }
 </style>
